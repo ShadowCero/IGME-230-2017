457cc70bd82e16f5689bef712fab877089dc2908
Yash Bagri <shadowcero474@gmail.com>
2017-12-18 05:01:21 -0500

    game

--- project1/js/classes.js
+++ project1/js/classes.js
@@ -2,7 +2,7 @@ class Player extends PIXI.Sprite{
     constructor(x=0,y=0){
         super(PIXI.loader.resources["media/player.png"].texture);
         this.anchor.set(.5,.5); //position scaling etc are now from center of sprite
-        this.scale.set(0.1);
+        this.scale.set(0.12);
         this.x = x;
         this.y = y;
         this.vx = 0;
